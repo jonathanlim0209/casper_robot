e125d550027b17f959e6add3bb07afb62cd50e4f
Jonathan <jonathanlim0209@gmail.com>
2025-05-25 22:28:04 +0800

    removed read encoder from arduino_comms

--- src/diffdrive_arduino/src/arduino_comms.cpp
+++ src/diffdrive_arduino/src/arduino_comms.cpp
@@ -22,18 +22,6 @@ void ArduinoComms::sendEmptyMsg()
     std::string response = sendMsg("\r");
 }
 
-void ArduinoComms::readEncoderValues(int &val_1, int &val_2)
-{
-    std::string response = sendMsg("e\r");
-
-    std::string delimiter = " ";
-    size_t del_pos = response.find(delimiter);
-    std::string token_1 = response.substr(0, del_pos);
-    std::string token_2 = response.substr(del_pos + delimiter.length());
-
-    val_1 = std::atoi(token_1.c_str());
-    val_2 = std::atoi(token_2.c_str());
-}
 
 void ArduinoComms::setMotorValues(int val_1, int val_2)
 {
