c70d691dbe4363c11a0e70f5caac5b78e3dd81f6
Jonathan <jonathanlim0209@gmail.com>
2025-05-28 23:09:06 +0800

    reordered robot description in launch_robot.launch.py

--- src/articubot_one/launch/launch_robot.launch.py
+++ src/articubot_one/launch/launch_robot.launch.py
@@ -31,6 +31,8 @@ def generate_launch_description():
     controller_params = os.path.join(get_package_share_directory('articubot_one'), 
                                      'config', 'my_controller.yaml')
 
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
     controller_manager = Node(
         package ='controller_manager',
         executable='ros2_control_node',
@@ -46,9 +48,6 @@ def generate_launch_description():
         )
 
     
-
-
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
