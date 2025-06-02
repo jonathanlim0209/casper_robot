b5817a7dba0a97e1327568dcc44a37eb2e31d6c1
Jonathan <jonathanlim0209@gmail.com>
2025-06-02 22:05:23 +0800

    Changed camera_stream package

---
+++ src/camera_stream/launch/camera_stream.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=[
+                'bash', '-c',
+                'rpicam-vid -t 0 -n --inline -o - | '
+                'gst-launch-1.0 fdsrc fd=0 ! h264parse ! '
+                'rtph264pay config-interval=1 pt=96 ! '
+                'udpsink host=192.168.1.36 port=5000'
+            ],
+            #shell=True,
+            #output='screen'
+        )
+    ])
