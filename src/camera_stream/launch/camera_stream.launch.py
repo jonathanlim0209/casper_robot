241ceeebbd9d155b4cabdb2a6c7ebd511c43b3db
Jonathan <jonathanlim0209@gmail.com>
2025-06-07 22:06:03 +0800

    add timestamp to gstream command to reduce latency

--- src/camera_stream/launch/camera_stream.launch.py
+++ src/camera_stream/launch/camera_stream.launch.py
@@ -9,10 +9,11 @@ def generate_launch_description():
             cmd=[
                 'bash', '-c',
                 'libcamera-vid -t 0 -n --inline -o - | '
-                'gst-launch-1.0 fdsrc fd=0 ! h264parse ! '
-                'rtph264pay config-interval=1 pt=96 ! '
-                'udpsink host=192.168.1.36 port=5000'
+                'gst-launch-1.0 fdsrc fd=0 do-timestamp=true ! '
+                'h264parse ! rtph264pay config-interval=1 pt=96 ! '
+                'udpsink host=192.168.1.36 port=5000 async=false sync=false'
             ],
+
             shell=True,
             output='screen'
         )
