0731ccd98cd19c43e3fea49a60a066201f795e87
Jonathan <jonathanlim0209@gmail.com>
2025-06-07 23:14:28 +0800

    changed the command for libcamera launch

--- src/camera_stream/launch/camera_stream.launch.py
+++ src/camera_stream/launch/camera_stream.launch.py
@@ -2,19 +2,18 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 
 def generate_launch_description():
-
-
     return LaunchDescription([
         ExecuteProcess(
             cmd=[
                 'bash', '-c',
-                'libcamera-vid -t 0 -n --inline -o - | '
-                'gst-launch-1.0 fdsrc fd=0 do-timestamp=true ! '
-                'h264parse ! rtph264pay config-interval=1 pt=96 ! '
-                'udpsink host=192.168.1.36 port=5000 async=false sync=false'
+                (
+                    'libcamera-vid -t 0 -n --inline -o - | '
+                    'gst-launch-1.0 fdsrc fd=0 do-timestamp=true ! '
+                    'h264parse ! rtph264pay config-interval=1 pt=96 ! '
+                    'udpsink host=192.168.1.36 port=5000 async=false sync=false'
+                )
             ],
-
-            shell=True,
             output='screen'
         )
     ])
+
