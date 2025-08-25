a493ab041e21fd9f3c09b902c4e247a49e0fa17f
Jonathan <jonathanlim0209@gmail.com>
2025-08-25 22:43:02 +0800

    added a launch from launch_robot.launch.py

--- src/articubot_one/launch/launch_robot.launch.py
+++ src/articubot_one/launch/launch_robot.launch.py
@@ -87,12 +87,22 @@ def generate_launch_description():
         )
     )
 
-    camera_stream_launch = IncludeLaunchDescription(
+    # camera_stream_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(
+    #             get_package_share_directory("camera_stream"),
+    #             'launch', 
+    #             'camera_stream.launch.py'
+    #         )
+    #     )
+    # )
+
+    camera_publisher_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
-                get_package_share_directory("camera_stream"),
+                get_package_share_directory("camera_publisher"),
                 'launch', 
-                'camera_stream.launch.py'
+                'camera_publisher.launch.py'
             )
         )
     )
@@ -124,5 +134,5 @@ def generate_launch_description():
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner,
-        camera_stream_launch
+        camera_publisher_launch
     ])
