fc1929065784615d0fb98d66bc01d64eef9e2d9b
Jonathan <jonathanlim0209@gmail.com>
2026-06-23 23:24:00 +0800

    Created new my_world.world and used as deafult

--- src/articubot_one/launch/launch_sim.launch.py
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -4,8 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 from launch.actions import RegisterEventHandler
@@ -37,13 +38,29 @@ def generate_launch_description():
     #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
     #     )
 
+
+    
+
+    world_file = os.path.join(get_package_share_directory(package_name),'worlds','my_world.world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=world_file,
+        description='World to load'
+        )
+    
+    world = LaunchConfiguration('world')
+
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    launch_arguments={
+                        'world': world,
+                        'extra_gazebo_args': ['--ros-args --params-file ', gazebo_params_file]
+                    }.items()
              )
     
 
@@ -116,6 +133,7 @@ def generate_launch_description():
     
     # Launch them all!
     return LaunchDescription([
+        world_arg,
         rsp,
         #joystick,
         #twist_mux,
