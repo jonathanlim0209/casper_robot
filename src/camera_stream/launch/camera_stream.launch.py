7a1cda53249e7f9d673f4d609895492085c038ee
Jonathan <jonathanlim0209@gmail.com>
2026-08-05 15:41:18 +0800

    changed the pipeline_cmd lowering bitrate to 400000 and increased keyframerate

--- src/camera_stream/launch/camera_stream.launch.py
+++ src/camera_stream/launch/camera_stream.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     # Zero-latency pipe: nobuffer + instant packet flushing
     pipeline_cmd = (
         "libcamera-vid -t 0 -n --inline --width 640 --height 480 --framerate 20 "
-        "--bitrate 600000 --g 20 --intra 10 --vflip --hflip -o - | "
+        "--bitrate 400000 --g 10 --intra 10 --vflip --hflip -o - | "
         "ffmpeg -re -i - -c copy -f rtsp -rtsp_transport tcp -max_delay 500000 -pkt_size 1316 -flush_packets 1 rtsp://localhost:8554/test"
     )
 
