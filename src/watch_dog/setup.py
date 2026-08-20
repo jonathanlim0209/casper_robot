d17fcac0667519480d24df0a2f7210dd5355d601
Jonathan <jonathanlim0209@gmail.com>
2026-08-20 12:32:11 +0800

    Fixed wrong implementation of watch_dog package

---
+++ src/watch_dog/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'watch_dog'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jonathan0209',
+    maintainer_email='jonathanlim0209@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'cmd_vel_watchdog = watch_dog.cmd_vel_watchdog:main',
+        ],
+    },
+)
